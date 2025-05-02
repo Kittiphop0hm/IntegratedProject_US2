9b2ca69e9be7b9b60d672c0bd82dd9d04b615998
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2025-05-02 14:15:35 +0700

    009-[create] ui Footer and responsive

--- frontend/src/views/LandingPage.vue
+++ frontend/src/views/LandingPage.vue
@@ -2,10 +2,12 @@
 import Navbar from './Navbar.vue';
 import TagAlert from './TagAlert.vue';
 import CTA from './CTA.vue';
+import Footer from './Footer.vue';
 </script>
 
 <template>
     <TagAlert/>
     <Navbar/>
     <CTA/>
+    <Footer/>
 </template>
