fb1341164ae424247a0a290db8b195bad5c5162c
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2025-05-02 13:36:54 +0700

    009-[update] responsive CTA

--- frontend/src/views/CTA.vue
+++ frontend/src/views/CTA.vue
@@ -4,13 +4,13 @@ import { ref } from 'vue';
 </script>
 
 <template>
-    <div class="w-full max-h-[632px]">
-        <div class="w-full h-[632px] bg-[#211C24] text-white overflow-hidden flex flex-row justify-between items-center px-[100px] xl:px-[160px]">
-            <div class="w-[50%]">
-                <h1 class="text-4xl font-bold">ITB-MSHOP</h1>
-                <p>The phone you’ve always wanted is just a click away. Grab it before it’s gone!</p>
+    <div class="w-full max-h-[700px]">
+        <div class="w-full h-[632px] bg-[#211C24] text-white overflow-hidden flex flex-row justify-between items-center md:px-[100px] xl:px-[160px] max-md:flex-col max-md:h-[700px] max-md:py-[120px] max-md:px-[20px]">
+            <div class="md:w-[50%] max-md:flex max-md:flex-col max-md:justify-center max-md:items-center">
+                <h1 class="text-4xl font-bold my-3 max-md:text-3xl">ITB-MSHOP</h1>
+                <p class="max-md:text-center max-md:text-sm">The phone you’ve always wanted is just a click away. Grab it before it’s gone!</p>
                 <router-link to="/sale-items">
-                    <button class="border border-white py-3 px-10 mt-5 cursor-pointer hover:opacity-70">
+                    <button class="border border-white w-[191px] h-[56px] px-10 mt-5 cursor-pointer hover:opacity-70">
                         Shop now
                     </button>
                 </router-link>
@@ -20,7 +20,4 @@ import { ref } from 'vue';
             </div>
         </div>
     </div>
-
-
-    
 </template>
