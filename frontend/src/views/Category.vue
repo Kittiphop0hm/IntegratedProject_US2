7bf76d5142944ab067a3fde736727cacf35d14c7
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2025-05-05 17:31:11 +0700

    009-[create] landing page ui Category Special Offers

---
+++ frontend/src/views/Category.vue
@@ -0,0 +1,90 @@
+<script setup>
+
+
+</script>
+
+<template>
+    <div class="w-full max-h-full">
+        <div class="w-full h-full flex flex-col justify-center items-center px-10 py-10 max-lg:px-5 max-lg:py-5">
+            <div class="mb-5 w-full flex flex-col justify-center items-center">
+                <h1 class="text-4xl font-bold">Special Offers</h1>
+                <div class="w-[50%] my-5 font-semibold flex flex-row justify-around items-center max-lg:w-full max-[426px]:flex-col">
+                    <p class="cursor-pointer">Apple</p>
+                    <p class="cursor-pointer">Samsung</p>
+                    <p class="cursor-pointer">Xiaomi</p>
+                    <p class="cursor-pointer">Huawei</p>
+                    <p class="cursor-pointer">ASUS</p>
+                    <p class="cursor-pointer">OPPO</p>
+                </div>
+            </div>
+
+            <div class="w-full h-[60vh] flex flex-row justify-center items-center gap-5 max-lg:h-full max-lg:flex-col">
+                <div class="w-[40%] h-full bg-gray-200 rounded-xl flex flex-col justify-around items-center p-10 cursor-pointer hover:shadow-xl max-lg:p-5 max-lg:w-full">
+                    <div class="rounded-2xl w-full">
+                        <div class="w-[10%] flex justify-center items-center bg-blue-500 rounded-2xl max-lg:w-[20%]">
+                            <p class="text-center text-sm text-white">New</p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="/images/Image.png" alt="Iphone" class="w-[100%] h-[300px] rounded-xl shadow-2xl">
+                    </div>
+                    <div class="flex flex-col justify-center items-center mt-5">
+                        <h1 class="text-center text-2xl font-bold">Iphone 14 Pro Max</h1>
+                        <p>6/512 GB</p>
+                    </div>
+                </div>
+
+                <div class="w-[40%] h-full grid grid-cols-2 gap-3 max-lg:w-full">
+                    <div class="w-full h-full bg-gray-200 rounded-xl flex flex-col justify-center items-center px-5 cursor-pointer hover:shadow-xl">
+                        <div class="flex flex-col justify-around items-center">
+                            <div class="my-2">
+                                <img src="/images/Iphone 14-pro-1.png" alt="">
+                            </div>
+                            <div class="my-2 flex flex-col justify-center items-center">
+                                <h1 class="text-center text-xl font-bold max-lg:text-sm">Iphone 14</h1>
+                                <p class="max-lg:text-sm">6/256 GB</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="w-full h-full bg-gray-200 rounded-xl flex flex-col justify-center items-center px-5 cursor-pointer hover:shadow-xl">
+                        <div class="flex flex-col justify-around items-center">
+                            <div class="my-2">
+                                <img src="/images/Iphone 14-pro-1.png" alt="">
+                            </div>
+                            <div class="my-2 flex flex-col justify-center items-center">
+                                <h1 class="text-center text-xl font-bold max-lg:text-sm">Iphone 14</h1>
+                                <p class="max-lg:text-sm">6/256 GB</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="w-full h-full bg-gray-200 rounded-xl flex flex-col justify-center items-center px-5 cursor-pointer hover:shadow-xl">
+                        <div class="flex flex-col justify-around items-center">
+                            <div class="my-2">
+                                <img src="/images/Iphone-14-proMax.png" alt="">
+                            </div>
+                            <div class="my-2 flex flex-col justify-center items-center">
+                                <h1 class="text-center text-xl font-bold max-lg:text-sm">Iphone 14 Pro Max</h1>
+                                <p class="max-lg:text-sm">6/512 GB</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="w-full h-full bg-gray-200 rounded-xl flex flex-col justify-center items-center px-5 cursor-pointer hover:shadow-xl">
+                        <div class="flex flex-col justify-around items-center">
+                            <div class="my-2">
+                                <img src="/images/Iphone-14-proMax.png" alt="">
+                            </div>
+                            <div class="my-2 flex flex-col justify-center items-center">
+                                <h1 class="text-center text-xl font-bold max-lg:text-sm">Iphone 14 Pro Max</h1>
+                                <p class="max-lg:text-sm">6/512 GB</p>
+                            </div>
+                        </div>
+                    </div>                   
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</template>
