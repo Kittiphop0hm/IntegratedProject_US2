ca7187edd736e95d678ecb354f2b65dda1917d3b
email <poramet.khan@kmutt.ac.th>
2025-05-15 20:34:53 +0700

    056- the SaleitemsList completely

--- frontend/src/components/model/SaleItemListTableModel.vue
+++ frontend/src/components/model/SaleItemListTableModel.vue
@@ -12,15 +12,15 @@ const props = defineProps({
   <table class="w-full border border-gray-300 text-sm">
     <thead class="bg-gray-100">
       <tr v-if="items.length > 0">
-        <th
-          v-for="(key, index) in Object.keys(items[0])"
-          :key="index"
-          class="border px-2 py-1"
-        >
-          {{ key }}
-        </th>
-        <th>Action</th>
-      </tr>
+  <th
+    v-for="(key, index) in Object.keys(items[0]).filter(key => key!== 'description')"
+    :key="index"
+    class="border px-2 py-1"
+  >
+    {{ key }}
+  </th>
+  <th class="border px-2 py-1">Action</th>
+</tr>
     </thead>
     <tbody>
       <slot
--- frontend/src/components/saleItem/SaleItemList.vue
+++ frontend/src/components/saleItem/SaleItemList.vue
@@ -1,29 +1,85 @@
 <script setup>
 import { ref, onMounted } from "vue";
-import { getItems } from "../../libs/fetchUtil.js";
-import SaleItemListTableModel from "../model/SaleItemListTableModel.vue"; 
-import Navbar from '../../views/Navbar.vue'
+import { getItems,deleteItemById } from "../../libs/fetchUtil.js";
+import SaleItemListTableModel from "../model/SaleItemListTableModel.vue";
+import Navbar from "../../views/Navbar.vue";
 import Search from "../Search.vue";
-
+import SaleItemDelete from "./SaleItemDelete.vue";
+import { useRouter,useRoute } from "vue-router";
+const route = useRoute();
 const saleItems = ref([]);
+const isDelete = ref(false);
+const router = useRouter();
+
+
+// const props = defineProps({
+//   typePopup: {
+//     validator(value) {
+//       return ["sale-items", "brands"].includes(value);
+//     },
+//     type: String,
+//     default:'Sale-items'
+//   },
+// });
 
 onMounted(async () => {
   try {
-    saleItems.value = await getItems(`${import.meta.env.VITE_APP_URL}/v1/sale-items`);
-    console.log(JSON.stringify(saleItems.value[0]))
+    saleItems.value = await getItems(
+      `${import.meta.env.VITE_APP_URL}/v1/sale-items`
+    );
   } catch (err) {
     console.error("Error fetching sale items:", err);
   }
 });
 
+const cancelDelete = () => {
+  isDelete.value = false
+}
+
+// const deleteStatus = ref(0)
+const deleteSaleItem = async (id) => {
+  try {
+    const deleteStatus = await deleteItemById(`${import.meta.env.VITE_APP_URL}/v1/sale-items`, id) 
+    if (deleteStatus === 204) {
+    const removeIndex = saleItems.value.findIndex(item => item.id === id )
+    saleItems.value.splice(removeIndex,1)
+      isDelete.value = false
+      router.push({ name: "SaleItemList", query: { alertDelete: "true" } });
+    }
+    if (deleteStatus === 404) {
+      router.push({ name: "SaleItemList", query: { alert404: "true" } });
+    }
+
+    
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+
 </script>
 
 <template>
   <div class="px-10">
-     <Navbar/>
-     <Search/>
+    <Navbar />
+    <Search />
 
-      <router-link :to="{ name: 'SaleItemAdd'  }">
+<div v-show="route.query.alertAdd || route.query.alertDelete || route.query.alert404" class=" p-10 pb-0">
+    <div class="bg-black/20 shadow-xl rounded px-8 pt-6 pb-8">
+      <div v-show="route.query.alertAdd || route.query.alertDelete">
+        <h1 class=" text-2xl text-green-400">Successfully</h1>
+        <br>
+        <p class="itbms-message"> The sale item has been <span class="text-green-400 "> {{ route.query.alertAdd ? 'successfully added.' : 'deleted.' }}</span></p>
+      </div>
+      <div v-show="route.query.alert404">
+        <h1 class=" text-2xl text-red-400">Error</h1>
+        <br>
+        <p class="itbms-message">The requested sale item does not exist.</p>
+      </div>
+    </div>
+  </div>
+
+    <router-link :to="{ name: 'SaleItemAdd' }">
       <button
         class="itbms-sale-item-add px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 mb-5 mt-5"
       >
@@ -34,15 +90,31 @@ onMounted(async () => {
     <SaleItemListTableModel :items="saleItems">
       <template #listItem="{ yourItem }">
         <tr class="itbms-row border-b">
-        <td class="border px-2 py-1 itbms-id">{{ yourItem.id ?? '-'}}</td>
-          <td class="border px-2 py-1 itbms-brand">{{ yourItem.brandName ?? '-'}}</td>
-          <td class="border px-2 py-1 itbms-model">{{ yourItem.model ?? '-'}}</td>
-          <td class="border px-2 py-1 itbms-ramGb">{{ yourItem.ramGb ?? '-'}}</td>
-          <td class="border px-2 py-1 itbms-storageGb">{{ yourItem.storageGb ?? '-'}}</td>
-          <td class="border px-2 py-1 itbms-screenSizeInch">{{ yourItem.screenSizeInch ?? '-'}}</td>
-          <td class="border px-2 py-1 itbms-color">{{ yourItem.color ?? '-' }}</td>
-          <td class="border px-2 py-1 itbms-price">{{ yourItem.price.toLocaleString() ?? '-'}}</td>
-          <td class="border px-2 py-1 itbms-quantity">{{ yourItem.quantity ?? '-' }}</td>
+          <td class="border px-2 py-1 itbms-id">{{ yourItem.id ?? "-" }}</td>
+          <td class="border px-2 py-1 itbms-brand">
+            {{ yourItem.brandName ?? "-" }}
+          </td>
+          <td class="border px-2 py-1 itbms-model">
+            {{ yourItem.model ?? "-" }}
+          </td>
+          <td class="border px-2 py-1 itbms-ramGb">
+            {{ yourItem.ramGb ?? "-" }}
+          </td>
+          <td class="border px-2 py-1 itbms-storageGb">
+            {{ yourItem.storageGb ?? "-" }}
+          </td>
+          <td class="border px-2 py-1 itbms-screenSizeInch">
+            {{ yourItem.screenSizeInch ?? "-" }}
+          </td>
+          <td class="border px-2 py-1 itbms-color">
+            {{ yourItem.color ?? "-" }}
+          </td>
+          <td class="border px-2 py-1 itbms-price">
+            {{ yourItem.price.toLocaleString() ?? "-" }}
+          </td>
+          <td class="border px-2 py-1 itbms-quantity">
+            {{ yourItem.quantity ?? "-" }}
+          </td>
           <td class="border px-2 py-1 text-center">
             <router-link
               :to="{ name: 'SaleItemEdit', params: { id: yourItem.id } }"
@@ -54,13 +126,15 @@ onMounted(async () => {
             <button
               class="bg-red-500 text-white px-2 py-1 rounded"
               data-testid="itbms-delete-button"
-              @click="console.log('Delete', yourItem.id)"
+              @click="isDelete=true "
             >
               Delete
             </button>
           </td>
         </tr>
+         <SaleItemDelete v-show="isDelete" @cancel-delete="cancelDelete" @delete-sale-item="deleteSaleItem(yourItem.id)"/>
       </template>
     </SaleItemListTableModel>
+    
   </div>
 </template>
