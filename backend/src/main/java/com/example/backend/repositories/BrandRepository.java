97227d07e0097ee3a65354bf72546312eba299bf
Teerapat Sangman <142310412+teerapatwee@users.noreply.github.com>
2025-05-19 15:41:17 +0700

    046-[Updated] Backend

--- backend/src/main/java/com/example/backend/repositories/BrandRepository.java
+++ backend/src/main/java/com/example/backend/repositories/BrandRepository.java
@@ -1,12 +1,13 @@
 package com.example.backend.repositories;
 
 import com.example.backend.entities.Brand;
+import com.example.backend.entities.SaleItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface BrandRepository extends JpaRepository<Brand, Integer> {
-    List<Brand> findAllByOrderByNameAsc();
     Boolean existsByName(String name);
     boolean existsByNameAndIdNot(String name, Integer id);
+    List<Brand> findAllByOrderByCreatedOn();
 }
