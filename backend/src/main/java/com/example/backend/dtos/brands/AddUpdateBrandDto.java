f803cb5ed44344b9e1a342dbd26b0bfd8ab9918e
Teerapat Sangman <142310412+teerapatwee@users.noreply.github.com>
2025-05-19 13:17:05 +0700

    046-[Updated] AddUpdateBrandDto.java

--- backend/src/main/java/com/example/backend/dtos/brands/AddUpdateBrandDto.java
+++ backend/src/main/java/com/example/backend/dtos/brands/AddUpdateBrandDto.java
@@ -10,4 +10,20 @@ public class AddUpdateBrandDto {
     private String websiteUrl;
     private String countryOfOrigin;
     private Boolean isActive;
+
+    public void setName(String name) {
+        if (name != null) {
+            name = name.trim();
+            this.name = name.isEmpty() ? null : name;
+        } else {
+            this.name = null;
+        }
+    }
+
+    public void setIsActive(Boolean isActive) {
+        if (isActive == null) {
+            isActive = true;
+        }
+        this.isActive = isActive;
+    }
 }
