2bf9c2541f59537bdb3bfda618ae5e05d350b50e
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2025-05-20 18:10:42 +0700

    update

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,11 +3,11 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
-    baseUrl: "http://localhost:5173",
-    baseAPI: "http://localhost:8080/itb-mshop",
+    // baseUrl: "http://localhost:5173",
+    // baseAPI: "http://localhost:8080/itb-mshop",
     experimentalRunAllSpecs: true,
-    // baseUrl: "http://ip24us2.sit.kmutt.ac.th",
-    // baseAPI: "http://ip24us2.sit.kmutt.ac.th:8080/itb-mshop",
+    baseUrl: "http://intproj24.sit.kmutt.ac.th/us2",
+    baseAPI: "http://intproj24.sit.kmutt.ac.th:8080/us2/itb-mshop",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
