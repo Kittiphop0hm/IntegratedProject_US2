fcc199852f0889161722ea17c9da7e6cc4279781
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2025-08-11 23:42:22 +0700

    update backend'

--- backend/src/main/java/com/example/backend/controllers/FileController.java
+++ backend/src/main/java/com/example/backend/controllers/FileController.java
@@ -38,7 +38,7 @@ public class FileController {
 //    }
 
     @PostMapping("")
-    public ResponseEntity<String> uploadFiles(@RequestParam("file") List<MultipartFile> files ,@RequestParam("saleId") Integer saleId) {
+    public ResponseEntity<String> uploadFiles(@RequestParam("files") List<MultipartFile> files ,@RequestParam("saleId") Integer saleId) {
         fileService.store(files , saleId);
         return ResponseEntity.status(HttpStatus.CREATED).body("Files are uploaded. " + files);
     }
