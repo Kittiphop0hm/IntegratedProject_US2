6d04c70314dfb85ec310458a175c84ea8eedbee7
Teerapat Sangman <142310412+teerapatwee@users.noreply.github.com>
2025-08-11 17:36:36 +0700

    046-[Updated] Uploaded Multi Files

--- backend/src/main/java/com/example/backend/entities/Picture.java
+++ backend/src/main/java/com/example/backend/entities/Picture.java
@@ -12,16 +12,22 @@ import lombok.Setter;
 @Table(name = "pictures")
 public class Picture {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Integer id;
 
     @Size(max = 45)
-    @Column(name = "name", length = 45)
-    private String name;
+    @Column(name = "originalName", length = 45)
+    private String originalName;
+
+    @Size(max = 45)
+    @Column(name = "realName", length = 45)
+    private String realName;
 
     @NotNull
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "sale_itemsId", nullable = false)
-    private SaleItem saleItems;
+    @JoinColumn(name = "salesId", nullable = false)
+    private SaleItem sales;
+
 
 }
