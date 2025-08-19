8afa55abdd2fe5d36e397676dd66c83e418e4010
SMarKleZ <142305093+SmarkJark03@users.noreply.github.com>
2025-08-19 21:22:58 +0700

    update

--- frontend/src/components/saleItem/SaleItemAddEdit.vue
+++ frontend/src/components/saleItem/SaleItemAddEdit.vue
@@ -244,7 +244,7 @@ const showFilename = (e) => {
   
   filenames.forEach((file) => {
     console.log(file.size);
-    if (file.size > 2100000) {
+    if (file.size > 2097152) {
       isOver2MB.value = true
       return
     }
