ace6f0ec5ec1d48873d9265913ddf7214e90ea73
Teerapat Sangman <142310412+teerapatwee@users.noreply.github.com>
2025-08-26 05:14:16 +0700

    046-[Finished] Email Verification

--- backend/src/main/java/com/example/backend/controllers/users/UserController.java
+++ backend/src/main/java/com/example/backend/controllers/users/UserController.java
@@ -3,8 +3,10 @@ package com.example.backend.controllers.users;
 import com.example.backend.dtos.users.RegisterFormDto;
 import com.example.backend.dtos.users.ResponseUserDto;
 import com.example.backend.dtos.users.ResponseUserPictureDto;
+import com.example.backend.services.users.JwtService;
 import com.example.backend.services.users.UserFileService;
 import com.example.backend.services.users.UserService;
+import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,13 +15,15 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v2/register")
+@RequestMapping("/v2/users")
 @CrossOrigin(origins = "${app.cors.allowed-origins}")
 public class UserController {
     @Autowired
     private UserService userService;
     @Autowired
     private UserFileService userFileService;
+    @Autowired
+    private JwtService jwtService;
 
     @GetMapping("/{userId}")
     public ResponseEntity<List<ResponseUserPictureDto>> findUserPictureByUserId(@PathVariable Integer userId) {
@@ -31,8 +35,16 @@ public class UserController {
         return ResponseEntity.ok(userService.findByEmail(email));
     }
 
-    @PostMapping("")
+    @PostMapping("/register")
     public ResponseEntity<ResponseUserDto> registerUser(@ModelAttribute RegisterFormDto userForm, @RequestParam(required = false) MultipartFile cardFrontImage, @RequestParam(required = false) MultipartFile cardBackImage) {
         return ResponseEntity.status(201).body(userService.createUser(userForm, cardFrontImage, cardBackImage));
+    }
+
+    @PostMapping("/verify-email")
+    public ResponseEntity<ResponseUserDto> verifyEmail(@RequestParam String token) {
+        Integer userId = jwtService.extractUserId(token);
+        String email = jwtService.extractEmail(token);
+        ResponseUserDto user = userService.verifyEmail(userId, email);
+        return ResponseEntity.ok(user);
     }
 }
