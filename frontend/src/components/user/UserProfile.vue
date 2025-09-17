5386840cd6be2bae31601dc7df97db6e3319687c
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2025-09-17 12:49:30 +0700

    009-update user profile

--- frontend/src/components/user/UserProfile.vue
+++ frontend/src/components/user/UserProfile.vue
@@ -8,6 +8,10 @@ const props = defineProps({
     type: Object,
     required: true
   },
+  userPictureCard: {
+    type: Array,
+    required: false
+  },
   isSuccess: {
     type: Boolean,
     default: false
@@ -21,21 +25,20 @@ const props = defineProps({
 const route = useRoute()
 const currentUser = ref({})
 const oldUser = ref({})
+const pictures = computed(() => props.userPictureCard)
 watchEffect(() => {
   props.user ? currentUser.value = {...props.user} : currentUser.value = {}
   props.user ? oldUser.value = {...props.user} : oldUser.value = {}
 })
 
+
 const enableEditBtn = computed(() => {
   return JSON.stringify(currentUser.value) !== JSON.stringify(oldUser.value)
 })
-
-
-
 </script>
 
 <template>
-  <div class="w-full h-[100vh] px-10 py-5 relative max-lg:h-full max-[450px]:p-0">
+  <div class="w-full h-[110vh] px-10 py-5 relative max-lg:h-full max-[450px]:p-0">
     <div class="absolute top-2 right-0">
 
       <div v-if="isSuccess" role="alert" class="alert alert-success rounded-r-none opacity-90">
@@ -162,7 +165,14 @@ const enableEditBtn = computed(() => {
                 <h1 class="font-semibold">User type</h1>
                 <div class="flex justify-between items-center bg-gray-200 p-3 rounded-lg">
                   <p class="max-[450px]:text-sm">{{ currentUser?.userType}}</p>
-                  <!-- <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="#000000" fill-rule="evenodd" d="M4.43 8.512a.75.75 0 0 1 1.058-.081L12 14.012l6.512-5.581a.75.75 0 0 1 .976 1.138l-7 6a.75.75 0 0 1-.976 0l-7-6a.75.75 0 0 1-.081-1.057Z" clip-rule="evenodd"/></svg> -->
+                </div>
+              </div>
+
+              <div>
+                <h1 class="font-semibold">Pictures</h1>
+                <div class="flex flex-col justify-center bg-gray-200 p-3 rounded-lg gap-y-2">
+                  <p class="max-[450px]:text-sm"><span class="font-semibold">Front:</span> {{ userPictureCard[0]?.filename }}</p>
+                  <p class="max-[450px]:text-sm"><span class="font-semibold">Back:</span> {{ userPictureCard[1]?.filename }}</p>
                 </div>
               </div>
             </div>
--- frontend/src/components/user/UserProfileManager.vue
+++ frontend/src/components/user/UserProfileManager.vue
@@ -1,21 +1,21 @@
 <script setup>
 import UserProfile from "@/components/user/UserProfile.vue";
 import {onMounted, ref} from "vue";
-import {editItem, getItemById} from "@/libs/fetchUtil.js";
+import {addItem, editItem, getItemById} from "@/libs/fetchUtil.js";
 import {decodeJWT} from "@/libs/decodeJWT.js";
 import { useRoute, useRouter } from "vue-router";
-import AlertMessageModel from "../model/AlertMessageModel.vue";
 
 const router = useRouter()
-const route = useRoute()
 const accessToken = sessionStorage.getItem('accessToken')
 const getUser = decodeJWT(accessToken)
 const user = ref({})
-
+const userPictureCard = ref([])
 
 onMounted(async () => {
   try {
     const data = await getItemById(`${import.meta.env.VITE_APP_URL}/v2/users`, getUser.id)
+    const pictures = await getItemById(`${import.meta.env.VITE_APP_URL}/v2/users/picture`, getUser.id)
+    userPictureCard.value = pictures
     user.value = data
   } catch (error) {
     console.error("Error fetching user data:", error);
@@ -25,12 +25,11 @@ onMounted(async () => {
 const isSuccess = ref(false)
 const isError = ref(false)
 const editUser = async (currentUser) => {
-  console.log(currentUser);
-  console.log(currentUser.id);
   if (currentUser) {
     const updateUser = await editItem(`${import.meta.env.VITE_APP_URL}/v2/users`, currentUser.id, currentUser) 
     router.push({name: 'UserProfile'})
     if (updateUser.status === 200) {
+        user.value = updateUser
         isSuccess.value = true
         setTimeout(() => {
           isSuccess.value = false
@@ -46,7 +45,7 @@ const editUser = async (currentUser) => {
 </script>
 
 <template>
-  <UserProfile :user="user" :isSuccess="isSuccess" :isError="isError" @updateUser="editUser"  />
+  <UserProfile :user="user" :userPictureCard="userPictureCard" :isSuccess="isSuccess" :isError="isError" @updateUser="editUser"  />
 </template>
 
 <style scoped>
