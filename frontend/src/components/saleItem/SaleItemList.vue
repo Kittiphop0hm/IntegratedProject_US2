735e276bad5ed956e312ea47c50ec61db263c8a1
Teerapat Sangman <142310412+teerapatwee@users.noreply.github.com>
2025-09-23 08:12:55 +0700

    Updated Missing Requirement

--- frontend/src/components/saleItem/SaleItemList.vue
+++ frontend/src/components/saleItem/SaleItemList.vue
@@ -40,7 +40,7 @@ const fetchData = async () => {
   try {
        const res = await getItemsWithToken(
 // `${import.meta.env.VITE_APP_URL}/v2/seller/${getUser.id}/sale-items?page=${pageNumber}&size=${pageSize}` ,
-`${import.meta.env.VITE_APP_URL}/v2/seller/${getUser.id}/sale-items?page=${pageNumber.value}&size=${pageSize.value}` ,
+`${import.meta.env.VITE_APP_URL}/v2/sellers/${getUser.id}/sale-items?page=${pageNumber.value}&size=${pageSize.value}` ,
     accessToken
       )
       pageObj.value = res
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,23 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
-import App from './App.vue'
-import router from './router'
+// import { createApp } from 'vue'
+// import { createPinia } from 'pinia'
+// // import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+// import App from './App.vue'
+// import router from './router'
 
-const app = createApp(App)
-const pinia = createPinia();
-pinia.use(piniaPluginPersistedstate);
-app.use(pinia)
+// const app = createApp(App)
+// const pinia = createPinia();
+// app.use(router)
+// app.use(pinia)
+// // pinia.use(piniaPluginPersistedstate);
 
-app.use(router)
 
-app.mount('#app')
+// app.mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import router from "./router";
+
+import { createPinia } from "pinia";
+createApp(App).use(router).use(createPinia()).mount("#app");
