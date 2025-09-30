19153e96e08b1e30ef680de9c58152879fa94989
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2025-10-01 01:06:12 +0700

    update format json response

--- backend/src/main/java/com/example/backend/dtos/orders/OrderItemDto.java
+++ backend/src/main/java/com/example/backend/dtos/orders/OrderItemDto.java
@@ -2,10 +2,12 @@ package com.example.backend.dtos.orders;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"no", "saleItemId", "price", "quantity", "description"})
 public class OrderItemDto {
     @JsonProperty("no")
     private Integer id;
