606127d4768ac506d95636e915031a98c96e8795
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2025-10-01 01:00:52 +0700

    009-fix json format

--- backend/src/main/java/com/example/backend/dtos/orders/OrderItemDto.java
+++ backend/src/main/java/com/example/backend/dtos/orders/OrderItemDto.java
@@ -1,12 +1,14 @@
 package com.example.backend.dtos.orders;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class OrderItemDto {
-    private Integer no;
+    @JsonProperty("no")
+    private Integer id;
     private Integer saleItemId;
     private Integer price;
     private Integer quantity;
