fcb4c697282e4cd071dd82ae2930d58efc6bb5b7
Teerapat Sangman <142310412+teerapatwee@users.noreply.github.com>
2025-10-25 11:19:08 +0700

    update Code Server

--- backend/src/main/java/com/example/backend/services/users/EmailService.java
+++ backend/src/main/java/com/example/backend/services/users/EmailService.java
@@ -11,6 +11,7 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
     //     http://intproj24.sit.kmutt.ac.th/us2/users/verify-email?token=%s
+    //              http://localhost:5173/verify-email?token=%s
     public void sendEmail(String to, String token) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
@@ -18,7 +19,6 @@ public class EmailService {
         String messageBody = """
                  Thank you for registration, Please Confirm your email to Get 2000 Robux Free!
      http://intproj24.sit.kmutt.ac.th/us2/users/verify-email?token=%s           
-   //              http://localhost:5173/verify-email?token=%s
                 """.formatted(token);
         message.setText(messageBody);
         mailSender.send(message);
