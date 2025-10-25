70beb0877b667e2426022d9496a404fda312d014
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2025-10-25 22:06:39 +0700

    009-re gallery

--- frontend/src/components/saleItem/SaleItemGallery.vue
+++ frontend/src/components/saleItem/SaleItemGallery.vue
@@ -35,24 +35,25 @@ function checkRole(yourItem) {
         :to="{ name: 'SaleItemDetail', params: { id: yourItem.id } }"
         class="block"
       >
-        <div class="itbms-row relative border rounded-xl shadow hover:shadow-md p-4 transition duration-300 ease-in-out cursor-pointer">
+        <div class="w-full itbms-row relative bg-[#9D8A7C] inset-shadow-sm inset-shadow-black rounded-xl shadow hover:-translate-y-1 active:translate-y-0 p-4 transition duration-300 ease-in-out cursor-pointer">
+          <div class="w-full flex justify-center items-center">
           <img
-            src="/images/iPhone14ProMax.jpg"
+            src="/images/carbon_no-image.png"
             alt="phone image"
-            class="w-full h-36 object-cover rounded-md mb-4"
+            class="w-25 h-25 object-cover rounded-md mb-4"
           />
-          <div class="itbms-brand text-sm text-red-500">
-            <!-- text-gray-900 -->
+          </div>
+          <div class="itbms-brand text-[#2D1E17] font-semibold">
             {{ yourItem.brandName }}
           </div>
-          <div class="text-lg font-semibold text-red-500 itbms-model">
+          <div class="text-lg font-semibold text-[#2D1E17] itbms-model">
             {{ yourItem.model }}
           </div>
           <div class="text-sm mt-1">
-            <span class="itbms-ramGb">
+            <span class="itbms-ramGb text-[#2D1E17]">
               {{ yourItem.ramGb ? yourItem.ramGb : "-" }} /
             </span>
-            <span class="itbms-storageGb">
+            <span class="itbms-storageGb text-[#2D1E17]">
               {{ yourItem.storageGb ? yourItem.storageGb : "-" }}
               <span class="itbms-storageGb-unit">GB</span></span
             >
@@ -61,19 +62,12 @@ function checkRole(yourItem) {
             <span class="itbms-price-unit">Baht:</span
             >{{ yourItem.price.toLocaleString("en-US") }}
           </div>
-
-          <!-- <button
-            v-if="yourItem.isOwnedByCurrentUser"
-            class="absolute bottom-3 right-3 bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:shadow-lg transition transform hover:-translate-y-1 active:translate-y-0"
-          >
-            Add to cart
-          </button> -->
           
           <button
           @click.stop.prevent="checkRole(yourItem)"
-            class="absolute bottom-3 right-3 bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:shadow-lg transition transform hover:-translate-y-1 active:translate-y-0"
+            class="absolute bottom-3 right-3 bg-[#523F31] cursor-pointer hover:opacity-90 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:shadow-lg transition transform hover:-translate-y-1 active:translate-y-0"
           >
-            Add to cart
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24"><path fill="#ffffff" d="M17 18c-1.11 0-2 .89-2 2a2 2 0 0 0 2 2a2 2 0 0 0 2-2a2 2 0 0 0-2-2M1 2v2h2l3.6 7.59l-1.36 2.45c-.15.28-.24.61-.24.96a2 2 0 0 0 2 2h12v-2H7.42a.25.25 0 0 1-.25-.25c0-.05.01-.09.03-.12L8.1 13h7.45c.75 0 1.41-.42 1.75-1.03l3.58-6.47c.07-.16.12-.33.12-.5a1 1 0 0 0-1-1H5.21l-.94-2M7 18c-1.11 0-2 .89-2 2a2 2 0 0 0 2 2a2 2 0 0 0 2-2a2 2 0 0 0-2-2Z"/></svg>
           </button>
         </div>
       </router-link>
--- frontend/src/components/saleItem/SaleItemManager.vue
+++ frontend/src/components/saleItem/SaleItemManager.vue
@@ -326,16 +326,16 @@ const checkRole = (yourItem) => {
     <router-link :to="{ name: 'SaleItemAdd' }">
       <button
         @click="savePreviousPath"
-        class="itbms-sale-item-add px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+        class="flex justify-center items-center gap-x-2 itbms-sale-item-add px-4 py-4 bg-[#523F31] text-white font-semibold cursor-pointer rounded-md hover:opacity-90"
       >
-        Add Sale Item
+        <p>Add SaleItem</p>
+        <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24"><g fill="none"><circle cx="12" cy="12" r="9" fill="#ffffff"/><path stroke="#000000" stroke-linecap="square" stroke-linejoin="round" stroke-width="1.2" d="M12 8v8m4-4H8"/></g></svg>
       </button>
     </router-link>
     <div>
-      <span>Show</span>
       <span class="ml-2">
         <select
-          class="bg-gray-500 p-2 border itbms-page-size"
+          defaultValue="Pick a color" className="select bg-[#523F31] text-white font-semibold"
           v-model.number="pageSize"
         >
           <option :value="5">5</option>
@@ -357,19 +357,19 @@ const checkRole = (yourItem) => {
   @addToCart="checkRole"
   ></SaleItemGallery>
 
-  <div class="p-10 pt-0" v-show="pageObj.totalPages > 1">
+  <div class="p-10 pt-0 text-white" v-show="pageObj.totalPages > 1">
     <button
-      class="itbms-page-first cursor-pointer bg-gray-600 pl-5 pr-5 pt-3 pb-3"
+      class="itbms-page-first cursor-pointer bg-[#523F31] pl-5 pr-5 pt-3 pb-3 rounded-l-lg"
       @click="pageNumber = 0"
-      :class="isFirst ? 'opacity-45' : 'bg-gray-600'"
+      :class="isFirst ? 'opacity-45' : 'bg-[#523F31]'"
       :disabled="isFirst"
     >
       First
     </button>
     <button
-      class="itbms-page-prev cursor-pointer bg-gray-600 pl-5 pr-5 pt-3 pb-3"
+      class="itbms-page-prev cursor-pointer bg-[#523F31] pl-5 pr-5 pt-3 pb-3"
       @click="pageNumber = pageNumber - 1"
-      :class="isFirst ? 'opacity-45' : 'bg-gray-600'"
+      :class="isFirst ? 'opacity-45' : 'bg-[#523F31]'"
       :disabled="isFirst"
     >
       Prev
@@ -383,24 +383,24 @@ const checkRole = (yourItem) => {
       <button
         class="pl-5 pr-5 pt-3 pb-3 cursor-pointer"
         :class="
-          index - 1 === pageNumber ? 'bg-green-600 text-white' : 'bg-gray-600'
+          index - 1 === pageNumber ? 'bg-[#9D8A7C] text-white' : 'bg-[#523F31]'
         "
       >
         {{ index }}
       </button>
     </span>
     <button
-      class="itbms-page-next bg-gray-600 pl-5 pr-5 pt-3 pb-3 cursor-pointer"
+      class="itbms-page-next bg-[#523F31] pl-5 pr-5 pt-3 pb-3 cursor-pointer"
       @click="pageNumber = pageNumber + 1"
-      :class="isLast ? 'opacity-45' : 'bg-gray-600'"
+      :class="isLast ? 'opacity-45' : 'bg-[#523F31]'"
       :disabled="isLast"
     >
       Next
     </button>
     <button
-      class="itbms-page-last bg-gray-600 pl-5 pr-5 pt-3 pb-3 cursor-pointer"
+      class="itbms-page-last rounded-r-lg bg-[#523F31] pl-5 pr-5 pt-3 pb-3 cursor-pointer"
       @click="pageNumber = pageObj.totalPages - 1"
-      :class="isLast ? 'opacity-45' : 'bg-gray-600'"
+      :class="isLast ? 'opacity-45' : 'bg-[#523F31]'"
       :disabled="isLast"
     >
       Last
